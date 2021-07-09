047569b8055314d809102e443257dd1117bf8eed
Tyler <tylerlack13@gmail.com>
2021-07-09 08:12:55 -0500

    adding Clion proj

---
+++ TylersCode/cmake-build-debug/CMakeFiles/TylersCode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TylersCode.dir/main.cpp.o"
+  "TylersCode.exe"
+  "TylersCode.pdb"
+  "libTylersCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TylersCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
